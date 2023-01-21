1b208239de9bdbc6b96302661f24ec132d39a14a
jarvisharishkumar <63893689+jarvisharishkumar@users.noreply.github.com>
2023-01-21 17:54:36 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-// run `node index.js` in the terminal
+const mqtt = require('mqtt');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const host = '172.24.112.1';
+const port = '1883';
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+
+const connectUrl = `mqtt://${host}:${port}`;
+const client = mqtt.connect(connectUrl, {
+  clean: true,
+  connectTimeout: 4000,
+  username: '',
+  password: '',
+  reconnectPeriod: 1000,
+});
+console.log(clientId);
+const topic = '/nodejs/mqtt';
+client.on('connect', () => {
+  console.log('Connected');
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`);
+  });
+  client.publish(
+    topic,
+    'nodejs mqtt test',
+    { qos: 0, retain: false },
+    (error) => {
+      if (error) {
+        console.error(error);
+      }
+    }
+  );
+});
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString());
+});
